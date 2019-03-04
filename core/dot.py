cfe823b2b2c95fcf0c76cf325cbf3ec01bcf4fc8
Djane Rey Mabelin <d.mabelin@arcanys.com>
2019-03-04 18:11:34 +0800

    service as table of IPs

--- core/dot.py
+++ core/dot.py
@@ -1,4 +1,5 @@
 from graphviz import Digraph
+from core.service_manager import Status
 
 
 class Dot:
@@ -6,10 +7,19 @@ class Dot:
         self.service_manager = service_manager
 
     def print(self, render_format='svg'):
-        dot = Digraph()
+        dot = Digraph(node_attr={'shape': 'plaintext'})
 
         for service_name, service_data in self.service_manager.services().items():
-            dot.node(service_name)
+            content = "<<table border='0' cellborder='1' cellspacing='0'>"
+            content += "<tr><td cellpadding='6'><b>{}</b></td></tr>".format(service_name)
+
             for dep in service_data['depends_on']:
                 dot.edge(service_name, dep)
+
+            for node, node_data in service_data['nodes'].items():
+                color = 'bgcolor="red"' if node_data['status'] != Status.UP else ''
+                content += "<tr><td cellpadding='6' {}>{}</td></tr>".format(color, node)
+
+            content += "</table>>"
+            dot.node(service_name, content)
         return dot.pipe(format=render_format)
